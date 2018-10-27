510d40cfd5d017828c9d64cd8db53711629e3487
Grace Toa <gracetoa29@gmail.com>
2018-10-27 23:59:45 +0200

    message remove

--- MyPlaces/AppDelegate.swift
+++ MyPlaces/AppDelegate.swift
@@ -16,10 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        let manager = ManagerPlaces.shared
-//        for place in manager.someTestPlace{
-//            manager.append(place)
-//        }
+
         return true
     }
 
